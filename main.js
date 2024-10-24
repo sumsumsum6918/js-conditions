82fa4940a6431be3e226f99cf50cbcd7a49daa0a
sum <tinafongfong@gmail.com>
2024-10-24 21:14:00 +0200

    compare 2 words

---
+++ main.js
@@ -0,0 +1,35 @@
+// let userName = prompt("Hi! What's your name?", "Poopoo Cat");
+
+// if (userName != null) {
+//   const greetingLines = `Hello ${userName}, welcome to the world!`;
+
+//   document.getElementById("greeting-user").innerHTML = greetingLines;
+//   console.log(greetingLines);
+// }
+
+function compareAlphabets() {
+  const word1 = document.getElementById("alphabet-1").value;
+  const word2 = document.getElementById("alphabet-2").value;
+  const displayWord = document.getElementById("display-smaller-alphabet");
+
+  if (word1 === word2) {
+    return (displayWord.innerHTML = `${word1} and ${word2} are the same words`);
+  }
+
+  const firstWord = getCompareIndex(word1, word2);
+
+  displayWord.innerHTML = `The word that appears first in the dictionary is ${firstWord}`;
+}
+
+function getCompareIndex(word1, word2) {
+  const displayWord = document.getElementById("display-smaller-alphabet");
+
+  const orderIndex = word1.localeCompare(word2);
+  console.log(orderIndex);
+
+  if (orderIndex < 0) {
+    return word1;
+  } else if (orderIndex > 0) {
+    return word2;
+  }
+}
